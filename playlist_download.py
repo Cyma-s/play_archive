8ac7f8c2b30d383be0403912bd27fb57bd690089
Cyma-s <ssol0319@naver.com>
2021-10-30 00:21:32 +0900

    edit : playlist json 변환

--- playlist_download.py
+++ playlist_download.py
@@ -1,4 +1,5 @@
 from pytube import Playlist
+import json
 
 v = "https://www.youtube.com/playlist?list="
 
@@ -9,14 +10,27 @@ def download_list(list_id):
     for video in pl.videos:
         # print(f"video title : {video.title}")
         first_video = video.streams.first()
-        dir_path = "E:\\Youtube\\"
+        dir_path = ".\\"
         first_video.download(dir_path)
+    make_json(pl)
 
 
-'''def show_progress(bytes_remaining, video):
-    percent = round((1 - bytes_remaining / video.filesize) * 100, 3)
-    if percent % 10 < 0.01:
-        print('% done...')'''
+def make_json(pl: Playlist):
+    json_dict = dict()
+    json_dict['list_id'] = pl.playlist_id
+    json_dict['list_name'] = pl.title
+    json_dict['videos'] = list()
+    video_urls = pl.video_urls
+    i = 0
+    for video in pl.videos:
+        video_info = dict()
+        video_info['video_id'] = video.video_id
+        video_info['video_title'] = video.title
+        video_info['video_url'] = video_urls[i]
+        json_dict['videos'].append(video_info)
+
+    with open(pl.playlist_id, "w", encoding='utf-8') as f:
+        json.dump(json_dict, f, indent='\t')
 
 
 playlist = input("플레이리스트 아이디? : ")
