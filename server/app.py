b657a6e127c1a744cc75b1ad4bc1e45c3e76b0bf
pula39 <programmed_life@naver.com>
2021-10-30 01:38:40 +0900

    Add temp func

--- server/app.py
+++ server/app.py
@@ -20,5 +20,23 @@ def my_melody():
 
     return my_res
 
+@app.route("/get_playlist")
+def get_my_playlist():
+    # http://localhost:5000/get_my_playlist?id=~~~~~~~~~
+
+    ## 뒤에 id를 가져와서
+    ## playlist_download.py 에 있는 코드 불러서
+    ## 인터넷에서 한번 가져온다.
+    ## 인터넷에서 가져온건 삭제된 것이 비어있다.
+    ## 하지만 우리 로컬에 저장되어있는 파일, 삭제된것이 안 비어 있을 수 있다.
+    ## 그 리스트를 쭉 만들때
+    ## 인터넷에서 이 곡은 삭제되었다. 하지만 우리가 정보를 들고있다를 포함한 정보를 쭉 보내준다.
+
+    pass
+
+@app.route("/backup_playlist")
+def backup_playlist():
+    pass 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
